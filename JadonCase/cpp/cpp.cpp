ce6e586e506de704a192fbbf462e0738eb9bbb2b
SEH00N <06psh0501@gmail.com>
2022-09-16 11:19:38 +0900

    jadoncase cpp & csharp

---
+++ JadonCase/cpp/cpp.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void replace(char& target, char value)
+{
+    target = value;
+}
+
+string solution(string s) {
+    string answer = s;
+
+    for(char& c : answer)
+        c = tolower(c);
+
+    replace(answer[0], toupper(answer[0]));
+
+    for (int i = 0; i < answer.length(); i++)
+        if (answer[i] == ' ' && i + 1 < answer.length())
+            replace(answer[i + 1], toupper(answer[i + 1]));
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution("console  dot write line ");
+
+    return 0;
+}
---
+++ JadonCase/csharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.solution("console  dot write liNe "));
+        }
+
+        public class Solution
+        {
+            public string solution(string s)
+            {
+                string answer = s;
+
+                answer = answer.ToLower();
+                answer = replaceAt(answer, 0, Char.ToUpper(answer[0]));
+
+                for(int i = 0; i < answer.Length; i++)
+                    if(answer[i] == ' ' && i + 1 < answer.Length)
+                        answer = replaceAt(answer, i + 1, Char.ToUpper(answer[i + 1]));
+
+                return answer;
+            }
+
+
+            public string replaceAt(string source, int index, char value)
+            {
+                char[] arr = source.ToCharArray();
+
+                arr[index] = value;
+
+                return String.Join(null, arr);
+            }
+        }
+    }
+}
