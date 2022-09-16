0f19954105e0bf3eccd4226d158bde0d5129c152
SEH00N <06psh0501@gmail.com>
2022-09-17 02:59:02 +0900

    BinaryTransformRepeat cpp

---
+++ BinaryTransformRepeat/cpp/cpp.cpp
@@ -0,0 +1,60 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string removeAll(string input, char target)
+{
+    string returnValue = "";
+
+    for (auto x : input)
+        if (x != target)
+            returnValue += x;
+
+    return returnValue;
+}
+
+string toBinary(int input)
+{
+    string returnValue = "";
+
+    while (input != 0)
+    {
+        returnValue.insert(0, to_string(input % 2));
+        input /= 2;
+    }
+
+    return returnValue;
+}
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    int repeatCount = 0;
+    int deleteCount = 0;
+
+
+    while (s.length() > 1)
+    {
+        repeatCount++;
+
+        deleteCount += removeAll(s, '1').length();
+
+        int length = removeAll(s, '0').length();
+
+        s = toBinary(length);
+    }
+
+    answer.push_back(repeatCount);
+    answer.push_back(deleteCount);
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> vec = solution("01110");
+    cout << vec[0] << ' ' << vec[1] << endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -24,3 +24,4 @@ Record of my coding test
     - 문제 : 0과 1로 이루어진 문자열 s가 "1"이 될 때까지 이진 변환을 가했을 때 이진 변환 횟수와 변환 과정에서 제거된 0의 개수를 배열에 담아 반환하는 함수를 완성하세요.
     - js - replaceAll 함수 통해 0을 모두 지운 후 toString(2)를 통해 2진수로 바꾸는 과정을 반복하여 해결하였습니다.
     - py - replace 함수 통해 0을 모두 지운 후 bin 함수를 활용하여 2진수로 바꾼 후 replace를 통해 0b(2진수 표현 형식)를 지우는 과정을 반복하여 해결하였습니다. 
+    - cpp - removeAll(string input, char target)라는 input의 target을 모두 지운 문자열을 반환하는 메소드와 toBinary(int input)라는 input의 2진수 값을 반환하는 메소드를 만들어 해결하였습니다.
