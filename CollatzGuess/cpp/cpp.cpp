6972576ceacf2879ea4fe887611908bd52410a98
SEH00N <06psh0501@gmail.com>
2022-09-21 16:15:30 +0900

    CollatzGuess

---
+++ CollatzGuess/cpp/cpp.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long temp = num;
+
+    while (temp != 1 && answer < 500)
+    {
+        temp = temp % 2 == 0 ? temp / 2 : temp * 3 + 1;
+        answer++;
+    }
+
+    if (answer == 500)
+        answer = -1;
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(10) << endl;
+
+    return 0;
+}
---
+++ CollatzGuess/csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            Console.WriteLine(s.solution(10));
+        }
+
+        public class Solution
+        {
+            public int solution(int num)
+            {
+                int answer = 0;
+                long temp = num;
+
+                while (temp != 1)
+                {
+                    if (answer == 500)
+                    {
+                        answer = -1;
+                        break;
+                    }
+
+                    temp = temp % 2 == 0 ? temp / 2 : temp * 3 + 1;
+                    answer++;
+                }
+
+                return answer;
+            }
+        }
+    }
+}
