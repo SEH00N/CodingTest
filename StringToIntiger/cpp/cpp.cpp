ec83ae84bfa56668af3569aa0073272924c99e36
SEH00N <06psh0501@gmail.com>
2022-09-21 16:06:55 +0900

    string to intiger

---
+++ StringToIntiger/cpp/cpp.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s)
+{
+	int answer = stoi(s);
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution("10") << endl;
+
+	return 0;
+}
---
+++ StringToIntiger/csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.solution("10"));
+        }
+
+        public class Solution
+        {
+            public int solution(string s)
+            {
+                int answer = int.Parse(s);
+                return answer;
+            }
+        }
+    }
+}
