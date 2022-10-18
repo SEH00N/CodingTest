42d1c3b76c98cadb9252930877fce6229edc0ef7
SEH00N <06psh0501@gmail.com>
2022-10-18 22:24:24 +0900

    Update Program.cs

--- Programmers/AdditionOfMatrices/csharp/Program.cs
+++ Programmers/AdditionOfMatrices/csharp/Program.cs
@@ -14,24 +14,23 @@ namespace csharp
             foreach (int x in result)
                 Console.Write(x + " ");
         }
-    }
-}
 
-
-public class Solution
-{
-    public int[,] solution(int[,] arr1, int[,] arr2)
-    {
-        int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)];
-
-        for (int i = 0; i < arr1.GetLength(0); i++)
+        public class Solution
         {
-            int[] tempArr = new int[arr1.GetLength(1)];
+            public int[,] solution(int[,] arr1, int[,] arr2)
+            {
+                int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)];
 
-            for (int j = 0; j < arr1.GetLength(1); j++)
-                answer[i, j] = arr1[i, j] + arr2[i, j];
+                for(int i = 0; i < arr1.GetLength(0); i++)
+                {
+                    int[] tempArr = new int[arr1.GetLength(1)];
+
+                    for(int j = 0; j < arr1.GetLength(1); j++)
+                        answer[i, j] = arr1[i, j] + arr2[i, j];
+                }
+
+                return answer;
+            }
         }
-
-        return answer;
     }
 }
