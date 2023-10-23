492633bf3d5183f77b0a1b84efca6dd8e68e1aa0
SEH00N <06psh0501@gmail.com>
2023-10-23 12:57:19 +0900

    웅까 꿍까

--- Baekjoon/BaekjoonCodingTest/BaekjoonCodingTest/웅까꿍까.cpp
+++ Baekjoon/BaekjoonCodingTest/BaekjoonCodingTest/웅까꿍까.cpp
@@ -1,73 +1,140 @@
+#include <iostream>
+
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 
-struct TreeNode
-{
+struct Song {
 public:
-	TreeNode(int value) : left{ nullptr }, right{ nullptr }, value{ value } {}
+    Song(std::string genre, int plays, int id) : genre{ genre }, plays{ plays }, id {id} {}
 
 public:
-	TreeNode* left;
-	TreeNode* right;
-	int value;
+    std::string genre;
+    int plays;
+    int id;
 };
 
-TreeNode* CreateTree(std::vector<std::vector<int>>& nodeinfo, int begin, int end)
-{
-	if (begin > end)
-		return nullptr;
-
-	int current = begin;
-	int maxHeight = nodeinfo[current][1];
-
-	for (int i = begin; i < end; ++i)
-	{
-		if (nodeinfo[i][1] > maxHeight)
-		{
-			maxHeight = nodeinfo[i][1];
-			current = i;
-		}
-	}
-
-	TreeNode* node = new TreeNode(nodeinfo[current][2]);
-	node->left = CreateTree(nodeinfo, begin, current - 1);
-	node->right = CreateTree(nodeinfo, current + 1, end);
+bool cmp(const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+    if (a.second == b.second) return a.first > b.first;
+    return a.second > b.second;
 }
 
-void PreOrder(std::vector<std::vector<int>>& answer, TreeNode* node)
-{
-	if (node == nullptr)
-		return;
-
-	answer[0].push_back(node->value);
-	PreOrder(answer, node->left);
-	PreOrder(answer, node->right);
+bool songCmp(const Song& left, const Song& right) {
+    return left.plays > right.plays;
 }
 
-void PostOrder(std::vector<std::vector<int>>& answer, TreeNode* node)
-{
-	if (node == nullptr)
-		return;
+std::vector<int> solution(std::vector<std::string> genres, std::vector<int> plays) {
+    std::vector<int> answer;
 
-	PostOrder(answer, node->left);
-	PostOrder(answer, node->right);
-	answer[1].push_back(node->value);
-}
+    std::unordered_map<std::string, int> chart;
+    std::unordered_map<std::string, std::vector<Song>> songs;
 
-std::vector<std::vector<int>> solution(std::vector<std::vector<int>> nodeinfo) {
-    std::vector<std::vector<int>> answer;
-	
-	for (int i = 0; i < nodeinfo.size(); ++i)
-		nodeinfo[i][2] = i + 1;
+    for (int i = 0; i < genres.size(); ++i)
+    {
+        std::string genre = genres[i];
+        int play = plays[i];
 
-	std::sort(nodeinfo.begin(), nodeinfo.end());
+        if (chart.find(genre) == chart.end())
+            chart.insert({ genre, 0 });
+        chart[genre] += play;
 
-    TreeNode* treeRoot = CreateTree(nodeinfo, 0, nodeinfo.size() - 1);
+        if (songs.find(genre) == songs.end())
+            songs.insert({ genre, std::vector<Song>() });
+        songs[genre].push_back(Song{ genre, play, i });
+    }
 
-	answer.resize(2, std::vector<int>());
+    std::vector<std::pair<std::string, int>> chartVec(chart.begin(), chart.end());
 
-	PreOrder(answer, treeRoot);
-	PostOrder(answer, treeRoot);
+    std::sort(chartVec.begin(), chartVec.end(), cmp);
+    for (const auto& i : chartVec)
+    {
+        std::vector<Song>& list = songs[i.first];
+        std::sort(list.begin(), list.end(), songCmp);
+
+        for (int j = 0; j < 2; ++j)
+            if(list.size() >= j + 1)
+               answer.push_back(list[j].id);
+    }
 
     return answer;
 }
+
+int main()
+{
+    auto a = solution({ "classic", "pop", "classic", "classic", "pop" }, { 500, 600, 150, 800, 2500 });
+    for (int i = 0; i < a.size(); ++i)
+        std::cout << a[i] << '\n';
+
+    return 0;
+}
+
+//#include <vector>
+//
+//std::string solution(std::string my_string, std::string letter) {
+//	while (true)
+//	{
+//		size_t i = my_string.find(letter);
+//		if (i == std::string::npos)
+//			break;
+//
+//		my_string.erase(i, 1);
+//	}
+//
+//	return my_string;
+//}
+
+//#include <vector>
+//#include <unordered_map>
+//
+//int solution(std::vector<std::vector<std::string>> clothes) {
+//    int answer = 1;
+//
+//    std::unordered_map<std::string, int> closet;
+//
+//    for (int i = 0; i < clothes.size(); ++i)
+//    {
+//        std::string type = clothes[i][1];
+//
+//        if (closet.find(type) == closet.end())
+//            closet.insert({ type, 1 });
+//
+//        closet[type]++;
+//    }
+//
+//    for (auto i = closet.begin(); i != closet.end(); ++i)
+//        answer *= i->second;
+//
+//    return answer - 1;
+//}
+//
+//#include <unordered_set>
+//
+//int main()
+//{
+//	int count;
+//	std::cin >> count;
+//
+//	std::unordered_set<std::string> inven;
+//	std::string character;
+//
+//	for(int i = 0; i < count; ++i)
+//	{
+//		std::cin >> character;
+//		inven.insert(character);
+//	}
+//
+//	std::cin >> count;
+//
+//	for (int i = 0; i < count; ++i)
+//	{
+//		std::cin >> character;
+//		if (inven.find(character) != inven.end())
+//			inven.erase(character);
+//	}
+//
+//	std::cout << inven.size() << '\n';
+//	for (auto i = inven.begin(); i != inven.end(); ++i)
+//		std::cout << *i << '\n';
+//
+//	return 0;
+//}
