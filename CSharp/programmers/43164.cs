f901e41509a3f1e37f3d516357010785488a0d73
SEH00N <06psh0501@gmail.com>
2024-10-04 16:53:31 +0900

    87946

--- CSharp/programmers/43164.cs
+++ CSharp/programmers/43164.cs
@@ -4,75 +4,75 @@ namespace CSharp.Programmers;
 
 class Solution43164
 {
-    static void Main(string[] args)
-    {
-        Solution43164 solution = new Solution43164();
+    // static void Main(string[] args)
+    // {
+    //     Solution43164 solution = new Solution43164();
 
-        {
-            string[] answer = solution.Solution(new string[,] {
-                { "ICN", "JFK" }, 
-                { "HND", "IAD" }, 
-                { "JFK", "HND" }
-            });
+    //     {
+    //         string[] answer = solution.Solution(new string[,] {
+    //             { "ICN", "JFK" }, 
+    //             { "HND", "IAD" }, 
+    //             { "JFK", "HND" }
+    //         });
 
-            foreach(string str in answer)
-                Console.Write($"{str} ");
-        }
+    //         foreach(string str in answer)
+    //             Console.Write($"{str} ");
+    //     }
 
-        Console.WriteLine("");
+    //     Console.WriteLine("");
 
-        {
-            string[] answer = solution.Solution(new string[,] {
-                { "ICN", "SFO" }, 
-                { "ICN", "ATL" }, 
-                { "SFO", "ATL" }, 
-                { "ATL", "ICN" }, 
-                { "ATL", "SFO" }
-            });
+    //     {
+    //         string[] answer = solution.Solution(new string[,] {
+    //             { "ICN", "SFO" }, 
+    //             { "ICN", "ATL" }, 
+    //             { "SFO", "ATL" }, 
+    //             { "ATL", "ICN" }, 
+    //             { "ATL", "SFO" }
+    //         });
 
-            foreach(string str in answer)
-                Console.Write($"{str} ");
-        }
+    //         foreach(string str in answer)
+    //             Console.Write($"{str} ");
+    //     }
 
-        Console.WriteLine("");
+    //     Console.WriteLine("");
 
-        {
-            // "만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다." 해당 조건만을 보고 무작정 정렬 후 순서대로 티켓을 사용하게 된다면
-            // 모든 티켓을 사용헤야 한다는 조건을 만족시키지 못한 채 종착지로 향하는 티켓을 사용하는 경우가 생길 수 있음.
+    //     {
+    //         // "만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다." 해당 조건만을 보고 무작정 정렬 후 순서대로 티켓을 사용하게 된다면
+    //         // 모든 티켓을 사용헤야 한다는 조건을 만족시키지 못한 채 종착지로 향하는 티켓을 사용하는 경우가 생길 수 있음.
 
-            string[] answer = solution.Solution(new string[,] {
-                { "ICN", "ABC" },
-                { "ICN", "BCA" },
-                { "BCA", "HDD" },
-                { "HDD", "ICN" }
-            });
+    //         string[] answer = solution.Solution(new string[,] {
+    //             { "ICN", "ABC" },
+    //             { "ICN", "BCA" },
+    //             { "BCA", "HDD" },
+    //             { "HDD", "ICN" }
+    //         });
 
-            // 다음과 같은 테스트케이스의 경우 무조건 정렬 후 순서대로 방문한다면 ICN -> ABC 로 끝나게 됨
-            // 하지만 모든 티켓을 사용해야 한다는 조건을 만족시켜 ICN -> BCA -> HDD -> ICN -> ABC 로 끝나게 해야함
+    //         // 다음과 같은 테스트케이스의 경우 무조건 정렬 후 순서대로 방문한다면 ICN -> ABC 로 끝나게 됨
+    //         // 하지만 모든 티켓을 사용해야 한다는 조건을 만족시켜 ICN -> BCA -> HDD -> ICN -> ABC 로 끝나게 해야함
 
-            // 한 번의 검증 과정을 추가하여 조건을 검사하자
+    //         // 한 번의 검증 과정을 추가하여 조건을 검사하자
 
-            foreach(string str in answer)
-                Console.Write($"{str} ");
-        }
+    //         foreach(string str in answer)
+    //             Console.Write($"{str} ");
+    //     }
 
-        Console.WriteLine("");
+    //     Console.WriteLine("");
 
-        {
-            string[] answer = solution.Solution(new string[,] {
-                { "EZE", "TIA" }, { "EZE", "HBA" }, { "AXA", "TIA" }, { "ICN", "AXA" }, 
-                { "ANU", "ICN" }, { "ADL", "ANU" }, { "TIA", "AUA" }, { "ANU", "AUA" }, 
-                { "ADL", "EZE" }, { "ADL", "EZE" }, { "EZE", "ADL" }, { "AXA", "EZE" }, 
-                { "AUA", "AXA" }, { "ICN", "AXA" }, { "AXA", "AUA" }, { "AUA", "ADL" }, 
-                { "ANU", "EZE" }, { "TIA", "ADL" }, { "EZE", "ANU" }, { "AUA", "ANU" }
-             });
+    //     {
+    //         string[] answer = solution.Solution(new string[,] {
+    //             { "EZE", "TIA" }, { "EZE", "HBA" }, { "AXA", "TIA" }, { "ICN", "AXA" }, 
+    //             { "ANU", "ICN" }, { "ADL", "ANU" }, { "TIA", "AUA" }, { "ANU", "AUA" }, 
+    //             { "ADL", "EZE" }, { "ADL", "EZE" }, { "EZE", "ADL" }, { "AXA", "EZE" }, 
+    //             { "AUA", "AXA" }, { "ICN", "AXA" }, { "AXA", "AUA" }, { "AUA", "ADL" }, 
+    //             { "ANU", "EZE" }, { "TIA", "ADL" }, { "EZE", "ANU" }, { "AUA", "ANU" }
+    //          });
 
-            foreach(string str in answer)
-                Console.Write($"{str} ");
-        }
+    //         foreach(string str in answer)
+    //             Console.Write($"{str} ");
+    //     }
 
-        Console.WriteLine("");
-    }
+    //     Console.WriteLine("");
+    // }
 
     class Destination
     {
---
+++ CSharp/programmers/87946.cs
@@ -0,0 +1,69 @@
+namespace CSharp.Programmers;
+
+class Solution87946
+{
+    // static void Main(string[] args)
+    // {
+    //     Solution87946 solution = new Solution87946();
+
+    //     Console.WriteLine(solution.Solution(80, new int[,] {
+    //         { 80, 20 }, { 50, 40 }, { 30, 10 }
+    //     }));
+    // }
+
+    public int Solution(int k, int[,] dungeons) 
+    {
+        int answer = 0;
+        int length = dungeons.GetLength(0);
+        bool[] visited = new bool[length];
+
+        for(int i = 0; i < length; ++i)
+        {
+            visited[i] = true;
+            k -= dungeons[i, 1];
+
+            int temp = Search(k, length, visited, dungeons);
+            if(temp > answer)
+                answer = temp;
+
+            visited[i] = false;
+            k += dungeons[i, 1];
+        }
+
+        return answer;
+    }
+
+    // 0, 60, 3, [true, false, false]
+    // 1, 20, 3, [true, true, false]
+    // -> 1반환
+
+    // 2, 50, 3, [true, false, true] -> 1 + 1 + 1반환
+    // 1, 10, 3, [true, true, true] -> 1 + 1반환
+    // -> 1반환
+
+    private int Search(int k, int length, bool[] visited, int[,] dungeons)
+    {
+        int count = 0;
+
+        for(int i = 0; i < length; ++i)
+        {
+            if(visited[i])
+                continue;
+
+            if(k < dungeons[i, 0])
+                continue;
+
+            visited[i] = true;
+            k -= dungeons[i, 1];
+
+            int temp = Search(k, length, visited, dungeons);
+            if(temp > count)
+                count = temp;
+
+            visited[i] = false;
+            k += dungeons[i, 1];
+        }
+
+        return ++count;
+    }
+}
